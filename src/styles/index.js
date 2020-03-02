586751c94141b2906390cdd2dc4bff407d0b0c41
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-03-02 10:45:02 -0600

    Initial commit -> react app initialiazed with the following dependencies: axios, styled-components, react-router-dom

---
+++ src/styles/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+// EXAMPLE
+/*
+export const MyStyle = styled.div`
+  // STYLES HERE
+`;
+
+IMPORT NON-DEFAULT EXPORT INTO OTHER FILES THIS WAY:
+  import { MyStyle } from './styles'; // BECAUSE THE FILE IS NAMED index, WE DON'T NEED TO INCLUDE THE FILE NAME IN THE PATH, BECAUSE THE index FILE WILL AUTOMATICALLY BE INCLUDED . . .
+*/
