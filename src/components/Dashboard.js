65e24e96f10cc60fc7cc2d0b5fc8c048af184f02
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-03-04 10:21:41 -0600

    Can add plants to dashboard.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,84 +3,121 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-const Dashboard = () => {
-//   const [user, setUser] = useContext(UserContext);
-//   const [plants, setPlants] = useContext(PlantContext);
-//   const id = localStorage.getItem('userID');
-//
-// // USER DETAILS
-//   useEffect(() => {
-//     axiosWithAuth()
-//       .get(`/api/users/${id}`)
-//       .then(res => {
-//         console.log('Data from useEffect: ', res.data);
-//         setUser(res.data);
-//       })
-//       .catch(err => `Error of type: ${err} has been thrown`);
-//
-//       axiosWithAuth()
-//         .get(`/api/plants`)
-//         .then(res => {
-//           setPlants(res.data);
-//           console.log('Data from plant: ', res.data);
-//         })
-//         .catch(err => `Error of type: ${err} has been thrown`);
-//   }, []);
-//
-//   const editUser = e => {
-//     e.preventDefault();
-//     axiosWithAuth()
-//       .put(`/api/users/${id}`, user)
-//       .then(res => {
-//         setUser(user);
-//       });
-//   }
-//
-// // PLANT DETAILS
-//   const addPlant = e => {
-//     e.preventDefault();
-//     axiosWithAuth()
-//       .post('/api/plants/', plants)
-//       .then(res => {
-//         console.log('Plant added');
-//       })
-//   };
-//
-//   const changeHandler = e => {
-//     setPlants([
-//       {
-//         ...plants,
-//         [e.target.name]: e.target.value
-//       }
-//     ]);
-//   }
+const Dashboard = props => {
+  const [user, setUser] = useState({
+    id:'',
+    username: '',
+    phone_number: ''
+  });
+  const [plants, setPlants] = useState([]);
+  const [addedPlant, setAddedPlant] = useState({
+    nickname: '',
+    species: '',
+    frequency: ''
+  });
+  const id = localStorage.getItem('userID');
+
+// USER DETAILS
+  useEffect(() => {
+    axiosWithAuth()
+      .get(`/api/users/${id}`)
+      .then(res => {
+        console.log('Data from useEffect: ', res.data);
+        setUser({
+          id: res.data.id,
+          username: res.data.username,
+          phone_number: res.data.phone_number
+        });
+      })
+      .catch(err => `Error of type: ${err} has been thrown`);
+  }, [id]);
+
+  const editUser = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .put(`/api/users/${id}`, {
+        username: user.username,
+        phone_number: user.phone_number
+      })
+      .then(() => {
+        console.log('Edit successfull');
+      });
+  }
+
+  const changeHandler = e => {
+    setUser({
+        ...user,
+        [e.target.name]: e.target.value
+      });
+  }
+
+// PLANT DETAILS
+  useEffect(() => {
+    axiosWithAuth()
+      .get('/api/plants')
+      .then(res2 => {
+        setPlants(res2.data);
+        console.log('Data from plant: ', res2.data);
+      })
+      .catch(err => `Error of type: ${err} has been thrown`);
+    }, [setPlants]);
+
+  const addPlant = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post('/api/plants/', {
+        nickname: addedPlant.nickname,
+        species: addedPlant.species,
+        frequency: addedPlant.frequency
+      })
+      .then(res => {
+        console.log('Plant added');
+        // props.history.push('/dashboard');
+      })
+  };
+
+  const plantChangeHandler = e => {
+    setAddedPlant({
+      ...addedPlant,
+      [e.target.name]: e.target.value
+    });
+  }
+
+
 
   return(
     <>
       <h2>Hello . . . I'm just a test page!</h2>
       <Link to="/login">Logout</Link>
-      {/*  <h3>My Plants:</h3>
-         {plants.map(plant => <p>{plant.nickname}</p>)}
-         <form onSubmit={addPlant}>
-          <input
-        type="text"
-              placeholder="Nickname"
-              name="nickname"
-              value={plants.nickname}
-              onChange={changeHandler}
-            />
-            <button type="submit">Add Plant</button>
-          </form>
-          <form onSubmit={editUser}>
-            <input
-              type="text"
-              placeholder="Username"
-              name="username"
-              // value={user.username}
-              // onChange={changeHandler}
-            />
-            <button type="submit">Edit Username</button>
-      </form> */}
+      <h3>My Plants:</h3>
+      {plants.map(plant => <p key={plant.nickname}>{plant.nickname}</p>)}
+      <form onSubmit={addPlant}>
+        <input
+          type="text"
+          placeholder="Nickname"
+          name="nickname"
+          value={addedPlant.nickname}
+          onChange={plantChangeHandler}
+        />
+        <button type="submit">Add Plant</button>
+      </form>
+      <form onSubmit={editUser}>
+        <input
+          type="text"
+          placeholder="Username"
+          name="username"
+          value={user.username}
+          onChange={changeHandler}
+        />
+        <input
+          type="text"
+          placeholder="New Number"
+          name="phone_number"
+          value={user.phone_number}
+          onChange={changeHandler}
+        />
+        <button type="submit">Edit Username</button>
+      </form>
     </>
   );
 }
