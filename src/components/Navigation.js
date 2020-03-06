0f7bc3cc5fa1d2135757675b8e6bec26625a499c
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-03-06 09:14:32 -0600

    Added link to landing page

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -12,7 +12,8 @@ const Navigation = props => {
 
   return (
   <Nav>
-    <LinkDiv>
+    <LinkDiv style={{alignItems:'baseline'}}>
+      <a href="https://build-week-water-my-plants-4.github.io/UI/" target="_blank" style={{marginRight:'1rem'}}>Home</a>
       {window.localStorage.getItem('token') && window.localStorage.getItem('userID') ? (
         <Link className="navlinks" to="/login" onClick={deleteToken}>Log Out</Link>
       ) : (
