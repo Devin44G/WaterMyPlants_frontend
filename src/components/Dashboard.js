2fcd73e6d358e357d8ac20f4d0c14cd70124669e
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-09-18 14:46:53 -0500

    Cleaned code -> Added message if unlogged users try to reach dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useEffect, useContext } from 'react';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 import { PLContext } from '../state/PLContext';
 import { GET_USER, GET_PLANTS, EDIT_USER, ADD_PLANT } from '../state/reducers/plReducer';
@@ -9,13 +9,6 @@ import { Motion, spring } from 'react-motion';
 const Dashboard = props => {
   const { data, dispatch } = useContext(PLContext);
   const user = JSON.parse(localStorage.getItem('user'));
-  // const [plants, setPlants] = useState([]);
-  // const [sun, setSun] = useState([]);
-  // const [addedPlant, setAddedPlant] = useState({
-  //   nickname: '',
-  //   species: '',
-  //   h2o_frequency: ''
-  // });
 
   useEffect(() => {
     if(user) {
@@ -60,70 +53,17 @@ const Dashboard = props => {
           .catch(err => `Error getting plants. Error: ${err}`);
       })
       .catch(err => console.log(`Error adding plant. ERROR: ${err}`));
-
-
-    // axiosWithAuth()
-    //   .get('/api/plants/mine')
-    //   .then( res => {
-    //     console.log(res.data);
-    //     // dispatch({ type: GET_PLANTS, payload: res.data });
-    //   })
-    //   .catch(err => `Error getting plants. Error: ${err}`);
-    // console.log(image);
   };
 
-  // const changeHandler = e => {
-  //   setUser({
-  //       ...user,
-  //       [e.target.name]: e.target.value
-  //     });
-  // }
-
-
-  // useEffect(() => {
-  //   axios
-  //   .get(`https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400
-  //   `)
-  //   .then(response => {
-  //     var test = response.data.results.sunrise;
-  //     //var keyArray = test.map(function(item) { return item["name"]; });
-  //     setSun(test);
-  //   //  console.log("dave" + test);
-  //   },[]);
-  //   axiosWithAuth()
-  //     .get('/api/plants')
-  //     .then(res2 => {
-  //       setPlants(res2.data);
-  //     })
-  //     .catch(err => `Error of type: ${err} has been thrown`);
-  //   }, [setPlants]);
-
-
-  //
-  // const plantChangeHandler = e => {
-  //   setAddedPlant({
-  //     ...addedPlant,
-  //     [e.target.name]: e.target.value
-  //   });
-  // }
-  //
-  // const SunTime = () => {
-  //   return (
-  //     <Motion defaultStyle={{ left: -100 }} style={{ left: spring(10) }}>
-  //       {val => <h1 style={{ position: 'relative', ...val }}>Sunrise {sun}{' '}
-  //       </h1>}
-  //     </Motion>
-  //   );
-  // };
   const token = window.localStorage.getItem('token');
 
   return(
     <div className="dashboard">
-      {!token ? 'You should log in' :
+      {!token ? <h3 style={{backgroundColor:'#8b4444', textDecoration:'none'}}>To view this page, you need to be <a href="/login">LOGGED IN</a></h3> :
       <DashboardContent data={data} editUser={editUser} addPlant={addPlant} />
       }
     </div>
   );
   }
 
-      export default Dashboard;
+export default Dashboard;
